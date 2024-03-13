18844434ff7971148cae1afa431cbf8cd5f79109
kammil1 <lis.kamil20@gmail.com>
2024-03-13 21:39:48 +0100

    up

---
+++ src/js/goit-modal-gallery.js
@@ -0,0 +1,48 @@
+export const galleryItems = [
+  {
+    preview: 'https://i.imgur.com/wAEXuEF.jpeg',
+    original: 'https://i.imgur.com/xk7Ksg9_d.webp?maxwidth=760&fidelity=grand',
+    description: 'Arkadiy Byts. Team Leader. The BOSS of the Cat Squad.',
+  },
+  {
+    preview: 'https://i.imgur.com/jkMGYAz.jpeg',
+    original: 'https://i.imgur.com/s6u8maF.jpeg',
+    description:
+      'Roksana Korban. The right hand of the leader. Everyone knows SHE really IS in charge of this chaos.',
+  },
+  {
+    preview: 'https://i.imgur.com/qFKVdxZ.jpeg',
+    original: 'https://i.imgur.com/rXy0Kvd.jpeg',
+    description: 'Filip Dąbrowski',
+  },
+  {
+    preview: 'https://i.imgur.com/wNP6IJl.jpeg',
+    original: 'https://i.imgur.com/Ir1wSyq.jpeg',
+    description: 'Filip Mantaj',
+  },
+  {
+    preview: 'https://i.imgur.com/YreeffS.jpeg',
+    original: 'https://i.imgur.com/pqRWEoD.jpeg',
+    description: 'Ewa Krzystolik',
+  },
+  {
+    preview: 'https://i.imgur.com/2dZm3ta.jpeg',
+    original: 'https://i.imgur.com/3I2J3lb.jpeg',
+    description: 'Jan Tacyn',
+  },
+  {
+    preview: 'https://i.imgur.com/W7GOYdK.jpeg',
+    original: 'https://i.imgur.com/PB97Y6G.jpeg',
+    description: 'Kamil Lis',
+  },
+  {
+    preview: 'https://i.imgur.com/X1i3cLG.jpeg',
+    original: 'https://i.imgur.com/6xri7za.jpeg',
+    description: 'Kamila Waniek',
+  },
+  {
+    preview: 'https://i.imgur.com/mWY54jx.jpeg',
+    original: 'https://i.imgur.com/KhS343W.jpeg',
+    description: 'Kamila Walkowska',
+  },
+];
