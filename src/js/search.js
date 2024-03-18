ce849d8f2c97bb20aa3e4861c78344220b7e695d
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-03-18 21:29:02 +0100

    update pagination current page

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,3 @@
-// dodać warunek sprawdzający, czy użytkownik nie wpisał tylko spacji
-// paginacja
-
 import axios from 'axios';
 
 const searchForm = document.querySelector('#search-form');
@@ -173,6 +170,12 @@ function renderPaginationButtons() {
         await fetchMovies();
         await scrollToTop();
       });
+
+      // Dodaj klasę CSS current-page do przycisku reprezentującego aktualną stronę
+      if (currentPage === i) {
+        pageButton.classList.add('current-page');
+      }
+      
       paginationContainer.appendChild(pageButton);
     }
   }
@@ -193,7 +196,7 @@ function renderPaginationButtons() {
   lastPageButton.addEventListener('click', async function () {
     currentPage = totalPages;
     await fetchMovies();
-    await crollToTop();
+    await scrollToTop();
   });
   paginationContainer.appendChild(lastPageButton);
 }
