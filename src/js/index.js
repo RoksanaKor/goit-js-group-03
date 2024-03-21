fcd7dee045648975750625fb15463cfb39843771
RoksanaKor <roksanakorban@gmail.com>
2024-03-21 20:21:05 +0100

    api key fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // EXAMPLE_API_REQUEST = https://api.themoviedb.org/3/movie/550?api_key=220ce7017a970ee5aafe37264f861af2;
 //TMDB Login: goit-js-group-03 hasło: goit-js-group-03
 
-API_KEY = '220ce7017a970ee5aafe37264f861af2';
+const API_KEY = '220ce7017a970ee5aafe37264f861af2';
 
 import {} from './search.js';
 import {} from './trending.js';
